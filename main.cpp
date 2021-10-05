fe43588959c82ae501cff29aa483c47c5f3eae6c
hi =) <88514677+VelocityStreak@users.noreply.github.com>
2021-10-05 17:55:36 +0100

    Added Functionality
    Added basic trigonometry.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "windows.h"
+#include <cmath>
 
 float rnum;
 float rnum2;
 float answer;
+
+long double trigonometry;
+
+long  double trigTodegree;
+
+long double result;
 
 void add() {
 
@@ -57,6 +64,36 @@ void multi() {
     //Sleep(8000);
 }
 
+void sin() {
+    std::cout << "What would you like to sin?" << std::endl;
+
+    std::cin >> trigonometry;
+
+    std::cout << "Sin(" << trigonometry << ") " << "equals: " << sin(trigonometry) << std::endl;
+    result = sin(trigonometry * 3.14159 / 180);
+    std::cout << "In Degrees: " << result << std::endl;
+}
+
+void cos() {
+    std::cout << "What would you like to cosine?" << std::endl;
+
+    std::cin >> trigonometry;
+
+    std::cout << "Cos(" << trigonometry << ") " << "equals: " << cos(trigonometry) << std::endl;
+    result = cos(trigonometry * 3.14159 / 180);
+    std::cout << "In Degrees: " << result << std::endl;
+}
+
+void tan() {
+    std::cout << "What would you like to tan?" << std::endl;
+
+    std::cin >> trigonometry;
+
+    std::cout << "Tan(" << trigonometry << ") " << "equals: " << tan(trigonometry) << std::endl;
+    result = tan(trigonometry * 3.14159 / 180);
+    std::cout << "In Degrees: " << result << std::endl;
+}
+
 int main() {
 
     int select;
@@ -68,6 +105,12 @@ int main() {
     std::cout << "Please choose from the list: [1] subtract " << std::endl;
     std::cout << "Please choose from the list: [2] divide " << std::endl;
     std::cout << "Please choose from the list: [3] mutliply " << std::endl;
+
+    std::cout << "==========| Advanced Features |==========" << std::endl;
+
+    std::cout << "Please choose from the list: [4] sin " << std::endl;
+    std::cout << "Please choose from the list: [5] cos " << std::endl;
+    std::cout << "Please choose from the list: [6] tan " << std::endl;
 
     std::cin >> select;
 
@@ -84,6 +127,17 @@ int main() {
     case 3:
         multi();
         break;
+    case 4:
+        sin();
+        break;
+    case 5:
+        cos();
+        break;
+    case 6:
+        tan();
+        break;
+    default:
+        std::cout << "Invalid Selection" << std::endl;
     }
     /*
     std::cout << "Do you want to run the program again? Enter yes for yes no for no [lower case]" << std::endl;
@@ -102,6 +156,6 @@ int main() {
     }
     */
     std::cout << "this is a work in progress, to use calc again pls relaunch <3 | Program made by VelocityStreak" << std::endl;
-    Sleep(5000);
+    Sleep(10000);
     return 0;
 }
